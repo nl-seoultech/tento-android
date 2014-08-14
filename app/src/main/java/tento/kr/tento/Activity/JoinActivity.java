8368a4c5b2482f129e8f67737ca048e78576e569
JinSeok Park <jinsuckelf@naver.com>
2014-08-14 10:18:19 +0900

    로그인 화면과 회원가입 화면 생성
    close #1
    close #2

---
+++ app/src/main/java/tento/kr/tento/Activity/JoinActivity.java
@@ -0,0 +1,49 @@
+package tento.kr.tento.Activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import tento.kr.tento.R;
+
+public class JoinActivity extends Activity implements View.OnClickListener {
+
+    Button btnCancle;
+    Button btnOkay;
+    EditText textJoinUserId;
+    EditText textJoinUserPassword;
+    EditText textJoinUserConfirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        btnOkay = (Button) this.findViewById(R.id.btnOkay);
+        btnCancle = (Button) this.findViewById(R.id.btnCancle);
+        textJoinUserId = (EditText) this.findViewById(R.id.JoinEmail);
+        textJoinUserPassword = (EditText) this.findViewById(R.id.JoinPassword);
+        textJoinUserConfirmPassword = (EditText) this.findViewById(R.id.JoinConfirmPassword);
+
+        btnOkay.setOnClickListener(this);
+        btnCancle.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.btnCancle:
+                //Cancle 버튼 눌렀을 때
+                finish();
+                break;
+
+            case R.id.btnOkay:
+                //Okay 버튼 눌렀을 때
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/tento/kr/tento/Activity/LoginActivity.java
@@ -0,0 +1,47 @@
+package tento.kr.tento.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import tento.kr.tento.R;
+
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    Button btnSignIn;
+    Button btnJoin;
+    EditText textUserEmail;
+    EditText txtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textUserEmail = (EditText) this.findViewById(R.id.UserEmail);
+        txtPassword = (EditText) this.findViewById(R.id.UserPassword);
+        btnJoin = (Button) this.findViewById(R.id.btnJoin);
+        btnSignIn = (Button) this.findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(this);
+        btnJoin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnJoin:
+                //Join 버튼 눌렀을 때
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnSignIn:
+                //Sign In 버튼 눌렀을 때
+                break;
+        }
+    }
+}
